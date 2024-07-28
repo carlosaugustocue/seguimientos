e8f2e80631d5ea38be7a59e5a9db2aa1e20275f3
Carlos Aranzazu <caaranzazu_230@cue.edu.co>
2024-07-28 12:52:41 -0500

    Se crean metodos estaticos.

--- src/com/estudio/variables/Main.java
+++ src/com/estudio/variables/Main.java
@@ -6,9 +6,15 @@ public class Main {
 
         System.out.println(vars);
         vars.setEdad((byte) 45);
+        vars.setSalarioCorto((short) 5000);
+        vars.setSalario(1500);
+        vars.setNumeroGrande(465683215L);
         vars.setPrecio(1835.25f);
+        vars.setPrecio(46545.454f);
+        vars.setValor(546878785454.87955);
+        vars.setLetra('L');
         vars.setEsVerdad(false);
-        System.out.println(vars.getEdad());
-        System.out.println(vars.toString());
+
+        System.out.println(vars);
     }
 }
