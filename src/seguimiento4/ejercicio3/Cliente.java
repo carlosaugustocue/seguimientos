dc4ec3487c51beff58ee81802c60a130215ea99c
Carlos Aranzazu <caaranzazu_230@cue.edu.co>
2024-08-19 18:03:18 -0500

    Se agrega el ejercicio 3 del 4 seguimiento.

---
+++ src/seguimiento4/ejercicio3/Cliente.java
@@ -0,0 +1,22 @@
+package seguimiento4.ejercicio3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Cliente extends Persona {
+    private List<Boleto> historialCompras;
+
+    public Cliente(String nombre, String identificacion, String direccion) {
+        super(nombre, identificacion, direccion);
+        this.historialCompras = new ArrayList<>();
+    }
+
+    public List<Boleto> obtenerHistorialCompras() {
+        return historialCompras;
+    }
+
+    public void agregarCompra(Boleto boleto) {
+        historialCompras.add(boleto);
+    }
+}
+
